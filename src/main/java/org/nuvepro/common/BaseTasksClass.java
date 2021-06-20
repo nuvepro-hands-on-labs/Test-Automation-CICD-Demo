e6b1a89407c99a63793809f68335af7626d37088
nuvepro-hands-on-labs <rishi@nuvepro.com>
2021-06-20 18:27:07 +0530

    Updated code and added driver

---
+++ src/main/java/org/nuvepro/common/BaseTasksClass.java
@@ -0,0 +1,257 @@
+package org.nuvepro.common;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.nuvepro.helperclasses.ClassFactory;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTasksClass {
+
+    private static Logger log = LogManager.getLogger(BaseTasksClass.class.getName());
+
+    private WebDriver driver;
+
+    public static String mvnBrowser, testBrowser;
+
+    private ClassFactory classFactory = new ClassFactory(driver);
+
+    private WebDriverWait wait;
+
+    public WebDriver getDriver(String browserName) {
+
+        getTestBrowser(browserName);
+
+        driver = classFactory.getDriverClass(testBrowser);
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        return driver;
+
+    }
+
+    public RemoteWebDriver getRemoteDriver(URL remoteUrl, DesiredCapabilities capabilities) {
+
+        System.out.println("Remote URL: " + remoteUrl);
+
+        RemoteWebDriver driver = new RemoteWebDriver(remoteUrl, capabilities);
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        return driver;
+
+    }
+
+    public DesiredCapabilities getChromeCapabilities() {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+
+        options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+        return capabilities;
+
+    }
+
+    public DesiredCapabilities getFirefoxCapabilities() {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        FirefoxOptions options = new FirefoxOptions();
+
+        capabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options);
+
+        return capabilities;
+
+    }
+
+    public String getTestBrowser(String browserName) {
+
+        mvnBrowser = System.getProperty("browser");
+
+        if (mvnBrowser == null) {
+
+            testBrowser = browserName;
+
+        } else {
+
+            testBrowser = mvnBrowser;
+        }
+
+        return testBrowser;
+
+    }
+
+    public Properties properties() {
+
+        Properties properties = new Properties();
+
+        try (FileInputStream inputFile = new FileInputStream(
+                System.getProperty("user.dir") + "/resources/properties.properties")) {
+
+            properties.load(inputFile);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return properties;
+
+    }
+
+    public void sleepTime(String nodeName, int timeToWaitInSeconds) {
+
+        System.out.println(nodeName + ": Pausing execution for " + timeToWaitInSeconds + "s.");
+
+        System.out.println(nodeName + ": Next execution count down timer started.");
+
+        try {
+
+            for (int i = timeToWaitInSeconds; i > 0; i--) {
+
+                if (i % 60 == 0) {
+
+                    System.out.println(nodeName + ": Execution resumes in " + i + "s...");
+
+                }
+
+                Thread.sleep(1000);
+
+            }
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public ArrayList<String> getHTMLLinks() {
+
+        XSSFWorkbook workBook = null;
+
+        ArrayList<String> activeLabUrls = new ArrayList<String>();
+
+        DataFormatter formatter = new DataFormatter();
+
+        try (FileInputStream inputFile = new FileInputStream(
+                System.getProperty("user.dir") + "/resources/datarepo/htmllinks.xlsx")) {
+
+            workBook = new XSSFWorkbook(inputFile);
+
+            XSSFSheet sheet = workBook.getSheet("links");
+
+            int rowCount = sheet.getPhysicalNumberOfRows();
+
+            for (int i = 0; i < rowCount - 1; i++) {
+
+                activeLabUrls.add(formatter.formatCellValue(sheet.getRow(i + 1).getCell(0)));
+
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return activeLabUrls;
+
+    }
+
+    public boolean checkSiteLinks(WebDriver driver, String nodeName, String link) {
+
+        driver.get(link);
+
+        ArrayList<WebElement> urls = new ArrayList<WebElement>();
+
+        urls.addAll(driver.findElements(By.tagName("a")));
+
+        HttpURLConnection huc = null;
+
+        for (int i = 0; i < urls.size(); i++) {
+
+            try {
+
+                System.out.println("\n\nURL is: " + urls.get(i).getAttribute("href"));
+
+                if (urls.get(i).getAttribute("href").startsWith("http")) {
+                    System.out.println("Checking URL is active: " + urls.get(i).getAttribute("href"));
+
+                    huc = (HttpURLConnection) new URL(urls.get(i).getAttribute("href")).openConnection();
+
+                    huc.setRequestMethod("HEAD");
+
+                    huc.connect();
+
+                    if (huc.getResponseCode() >= 400) {
+
+                        System.err.println("Link is broken.");
+
+                        return false;
+
+                    } else {
+
+                        System.out.println("Link is working.");
+
+                        return true;
+
+                    }
+
+                }
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public void clearInSecurePage(WebDriver driver) {
+
+        Actions actions = new Actions(driver);
+
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.ENTER).build().perform();
+
+    }
+
+    public WebElement getElement(WebElement element) {
+
+        return element;
+
+    }
+
+}
--- src/test/java/org/nuvepro/container/chrome/nodes/ChromeNode1.java
+++ src/test/java/org/nuvepro/container/chrome/nodes/ChromeNode1.java
@@ -2,28 +2,26 @@ package org.nuvepro.container.chrome.nodes;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.nuvepro.common.BaseTasks;
-import org.nuvepro.common.ManageUtilities;
+import org.nuvepro.common.BaseTasksClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
-public class ChromeNode1 extends BaseTasks {
+public class ChromeNode1 extends BaseTasksClass {
 
     private static Logger log = LogManager.getLogger(ChromeNode1.class.getName());
 
     private WebDriver driver = null;
 
-    private ManageUtilities manage = new ManageUtilities();
-
     private int link = 0;
 
+    @BeforeClass
     public void setupLocalDriver() {
 
         // Only to test the setup, local driver will be used.
@@ -32,18 +30,20 @@ public class ChromeNode1 extends BaseTasks {
 
         driver = getDriver(properties().getProperty("browser"));
 
-        driver.get(properties().getProperty("testURL"));
+        System.out.println(ChromeNode1.class.getSimpleName() + ": URL to load: " + getHTMLLinks().get(link));
+
+        driver.get(getHTMLLinks().get(link));
 
     }
 
-    @BeforeClass
+
     public void setupRemoteDriver() {
 
         System.out.println(ChromeNode1.class.getSimpleName() + ": Initializing REMOTE WEBDRIVER to run the tests remotely in Docker Containers.");
 
         try {
 
-            driver = getRemoteDriver(new URL(manage.getHTMLLinks().get(link)), getChromeCapabilities());
+            driver = getRemoteDriver(new URL(properties().getProperty("huburl")), getChromeCapabilities());
 
         } catch (MalformedURLException e) {
 
@@ -51,23 +51,38 @@ public class ChromeNode1 extends BaseTasks {
 
         }
 
-        System.out.println(ChromeNode1.class.getSimpleName() + ": URL to load: " + manage.getHTMLLinks().get(link));
+        System.out.println(ChromeNode1.class.getSimpleName() + ": URL to load: " + getHTMLLinks().get(link));
 
-        driver.get(manage.getHTMLLinks().get(link));
-
+        driver.get(getHTMLLinks().get(link));
 
     }
 
     @Test(enabled = true)
     public void chromeNode1() {
 
-        System.out.println(ChromeNode1.class.getSimpleName() + ": Waiting for URL to load in ChromeNode1.");
+        System.out.println(ChromeNode1.class.getSimpleName() + ": Waiting for Base URL to load.");
 
-        System.out.println(ChromeNode1.class.getSimpleName() + ": " + driver.getTitle());
+        System.out.println(ChromeNode1.class.getSimpleName() + ": Base URL Title: " + driver.getTitle());
 
-        // Modify the link here. It should be the clickable link given here.
+        System.out.println(ChromeNode1.class.getSimpleName() + ": Checking base URL status.");
 
-        manage.checkSiteLinks(driver, ChromeNode1.class.getSimpleName(), manage.getHTMLLinks().get(link));
+        if (checkSiteLinks(driver, ChromeNode1.class.getSimpleName(), getHTMLLinks().get(link))) {
+
+            System.out.println(ChromeNode1.class.getSimpleName() + ": Opening link: "
+                    + driver.findElement(By.xpath("//a[contains(@href,'http://')]")).getText());
+
+            driver.findElement(By.tagName("a")).click();
+
+            System.out.println(ChromeNode1.class.getSimpleName() + ": Checking other links in the page opened");
+
+            checkSiteLinks(driver, ChromeNode1.class.getSimpleName(), driver.getCurrentUrl());
+
+        } else {
+
+            System.out.println("Failed to access link.");
+
+            //Assert.fail();
+        }
 
         sleepTime(ChromeNode1.class.getSimpleName(), 10);
 
